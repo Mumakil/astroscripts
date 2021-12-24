7094feb4c0498ad55399a6f629874fcd222a7332
Otto Vehvilainen <otto.vehvilainen@smartly.io>
2021-12-24 22:29:19 +0200

    Update fits header edit script

--- bulk_edit_fits_headers.py
+++ bulk_edit_fits_headers.py
@@ -1,9 +1,18 @@
 import os
+import sys
 from astropy.io import fits
 
-files = os.listdir('./bulk/')
+if len(sys.argv) != 4:
+    print("Usage: python bulk_edit_fits_headers.py [dir] [headername] [value]")
+    exit(1)
+
+target = sys.argv[1]
+header = sys.argv[2]
+value = sys.argv[3]
+
+files = [f for f in os.listdir(target) if f.endswith(".fit") or f.endswith(".fits")]
+
+print(f"Setting {header}={value} on {len(files)} files")
 
 for f in files:
-    if f.endswith((".fit")):
-        # print(repr(fits.getheader("./bulk/" + f)))
-        fits.setval("./bulk/" + f, 'IMAGETYP', value='Dark')
+    fits.setval(target + f, header, value=value)
