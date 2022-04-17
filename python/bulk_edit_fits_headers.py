e2f03f9029b7e3e9b512660c4b36f20c534fe12d
Otto Vehvilainen <otto.vehvilainen@smartly.io>
2022-04-17 22:30:40 +0300

    Use glob patters instead of directory paths for bulk edit

--- python/bulk_edit_fits_headers.py
+++ python/bulk_edit_fits_headers.py
@@ -1,18 +1,19 @@
 import os
+import glob
 import sys
 from astropy.io import fits
 
 if len(sys.argv) != 4:
-    print("Usage: python bulk_edit_fits_headers.py [dir] [headername] [value]")
+    print("Usage: python bulk_edit_fits_headers.py [glob] [headername] [value]")
     exit(1)
 
 target = sys.argv[1]
 header = sys.argv[2]
 value = sys.argv[3]
 
-files = [f for f in os.listdir(target) if f.endswith(".fit") or f.endswith(".fits")]
+files = list(filter(os.path.isfile, glob.iglob(target, recursive=True)))
 
 print(f"Setting {header}={value} on {len(files)} files")
 
 for f in files:
-    fits.setval(target + f, header, value=value)
+    fits.setval(f, header, value=value)
