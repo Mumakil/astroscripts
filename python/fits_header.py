e172123c5dc2e31d565c7391a03f39a255143d94
Otto Vehvilainen <otto.vehvilainen@smartly.io>
2022-04-17 22:40:14 +0300

    Error handling for header inspection script

--- python/fits_header.py
+++ python/fits_header.py
@@ -5,12 +5,24 @@ from astropy.io import fits
 target = sys.argv[1]
 
 if len(sys.argv) == 2:
-  hdul = fits.open(target)
+  try:
+    hdul = fits.open(target)
+  except:
+    print ("Error opening the file")
+    exit(1)
   headers = hdul[0].header
-  print(list(headers.keys()))
+  try:
+    print("\n".join(list(headers.keys())))
+  except:
+    print("No such header")
+    exit(1)
 elif len(sys.argv) == 3:
   header = sys.argv[2]
-  value = fits.getval(target, header)
+  try:
+    value = fits.getval(target, header)
+  except:
+    print("Could not open file or no such header")
+    exit(1)
   print(value)
 else:
   print("Usage: python fits_header.py [file] [header]")
